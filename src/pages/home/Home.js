e45c158ab3a4bad8a70d75850f3639d32c997409
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-06-12 19:22:58 +0530

    Transaction form

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,10 +1,14 @@
 // styles
-import styles from './Home.module.css'
+import TransactionForm from "../../components/TransactionForm";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
     </div>
-  )
+  );
 }
