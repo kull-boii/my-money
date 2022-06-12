8a4db7d3d4671c732cb25f05e2a8856f736753ff
Aadi Manchekar <aadiprakash.manchekar2019@vitstudent.ac.in>
2022-06-12 14:57:17 +0530

    using useLogout hook

---
+++ src/hooks/useLogout.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { projectAuth } from "../firebase/config";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const logout = async () => {
+    setIsPending(true);
+    setError(null);
+
+    try {
+      await projectAuth.signOut();
+
+      dispatch({ type: "LOGOUT" });
+
+      setIsPending(false);
+      setError(null);
+    } catch (err) {
+      console.log(err.message);
+      setError(err.message);
+      setIsPending(false);
+    }
+  };
+
+  return { logout, error, isPending };
+};
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { projectAuth } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
 
